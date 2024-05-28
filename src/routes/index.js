ecf5234c249cfe9c4c7f7fb14fa3a421564bf580
Manoj Dhami <mdhami7@myseneca.ca>
2024-05-27 20:27:09 -0400

    Use the respose.js success and error in all https routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -10,6 +10,7 @@ const router = express.Router();
 
 // Our authentication middleware
 const { authenticate } = require("../auth");
+const { createSuccessResponse } = require("../response");
 
 /**
  * Expose all of our API routes on /v1/* to include an API version.
@@ -27,13 +28,14 @@ router.get("/", (req, res) => {
   res.setHeader("Cache-Control", "no-cache");
 
   // Send a 200 'OK' response with info about our repo
-  res.status(200).json({
+  const data = {
     status: "ok",
     author,
     // TODO: change this to use your GitHub username!
     githubUrl: "https://github.com/Dmanoj07/fragments",
     version,
-  });
+  };
+  res.status(200).json(createSuccessResponse(data));
 });
 
 module.exports = router;
