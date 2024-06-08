e12de040354dc52cdad283040192432cefb7a6e3
Manoj Dhami <mdhami7@myseneca.ca>
2024-06-08 12:42:01 -0500

    wriiten test for memory.tset.js

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -6,10 +6,8 @@ const { createSuccessResponse } = require("../../response");
  * Get a list of fragments for the current user
  */
 module.exports = (req, res) => {
-  // TODO: this is just a placeholder. To get something working, return an empty array...
   const data = {
     status: "ok",
-    // TODO: change me
     fragments: [],
   };
   res.status(200).json(createSuccessResponse(data));
