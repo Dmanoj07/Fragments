804dbb4fc9001eaf4f07f7c1152b0373f426d180
Manoj Dhami <mdhami7@myseneca.ca>
2024-08-12 13:12:50 -0400

    Resolve lint error

--- src/routes/api/post.js
+++ src/routes/api/post.js
@@ -1,63 +1,3 @@
-// const contentType = require("content-type");
-// const { Fragment } = require("../../model/fragment");
-// const logger = require("../../logger");
-// const { createErrorResponse } = require("../../response");
-
-// module.exports = async (req, res) => {
-//   try {
-//     const { type, parameters } = contentType.parse(req.headers["content-type"]);
-//     const fullType =
-//       type + (parameters.charset ? `; charset=${parameters.charset}` : "");
-
-//     if (!Fragment.isSupportedType(fullType)) {
-//       return res
-//         .status(415)
-//         .json(createErrorResponse(415, "Unsupported Content Type"));
-//     }
-
-//     if (!req.body || req.body.length === 0) {
-//       throw new Error("Empty fragment body");
-//     }
-
-//     const fragmentData = Buffer.isBuffer(req.body)
-//       ? req.body
-//       : Buffer.from(req.body);
-//     const fragmentSize = fragmentData.length;
-
-//     logger.debug(
-//       {
-//         ownerId: req.user,
-//         type: fullType,
-//         size: fragmentSize,
-//         data: fragmentData.toString("utf-8").substring(0, 100), // Log first 100 chars
-//       },
-//       "Creating new fragment"
-//     );
-
-//     const newFragment = new Fragment({
-//       ownerId: req.user,
-//       type: fullType,
-//       size: fragmentSize,
-//     });
-
-//     await newFragment.save();
-//     await newFragment.setData(fragmentData);
-
-//     const location = `${req.protocol}://${req.get("host")}/v1/fragments/${newFragment.id}`;
-//     res.setHeader("Location", location);
-//     res.status(201).json({
-//       status: "ok",
-//       fragment: newFragment,
-//     });
-//   } catch (err) {
-//     logger.error(`Error creating fragment: ${err.message}`);
-//     const statusCode = err.message.includes("Unsupported Content Type")
-//       ? 415
-//       : 400;
-//     res.status(statusCode).json(createErrorResponse(statusCode, err.message));
-//   }
-// };
-
 const contentType = require("content-type");
 const { Fragment } = require("../../model/fragment");
 const logger = require("../../logger");
@@ -97,7 +37,8 @@ module.exports = async (req, res) => {
     if (fullType === "application/json") {
       try {
         JSON.parse(fragmentData.toString());
-      } catch (error) {
+      } catch (jsonError) {
+        logger.error(`Invalid JSON: ${jsonError.message}`);
         throw new Error("Invalid JSON");
       }
     }
